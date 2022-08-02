50237babf0ef854f6c82df4fc6400ecdb08e57cf
Comamoca <comamoca.dev@gmail.com>
2022-08-02 16:59:09 +0900

    :seedling: (プロジェクトのセットアップ)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import { useState } from "react";
+// import logo from "./logo.svg";
+// import poweredBy from "./powered-by-vitawind-dark.png";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <article className="prose">
+    <h1>Hello!</h1>
+    </article>
+    </>
+  );
+}
+
+export default App;
